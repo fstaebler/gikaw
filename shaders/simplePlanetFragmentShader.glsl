1c32c035437fdd9d8c2cf6bcf34cd3445834a4ee
Valentin Buck <apexys@toppoint.de>
2015-05-29 18:57:16 +0200

    Added sampler for equirectangular projection

--- shaders/simplePlanetFragmentShader.glsl
+++ shaders/simplePlanetFragmentShader.glsl
@@ -1,43 +1,55 @@
+#define M_PI 3.1415926535897932384626433832795
 uniform vec3 location;
 uniform float radius;
 uniform vec4 color;
 
 varying vec2 screenPos;
 
-void main(){
 
-gl_FragColor = vec4(0.0, screenPos, 1.0);
-
-vec3 viewPos;
-vec3 c;
-float radius;
-vec3 o;
-float distance;
-vec3 l;
-//Hier den Richtungsvektor normalisieren (Länge berechnen und durch Länge teilen)
-l = normalize(l);
-/*Ab hier sei gegeben:
+vec4 sampleColor(vec3 viewPos, vec3 planetPos, float planetRadius){
+  vec3 direction = normalize(planetPos - viewPos);
+  /*Ab hier sei gegeben:
   Kugel durch ||x - c||² = r² mit
-    c = center
-    r = radius
-    x = einzusetzender Punkt
+  c = center
+  r = radius
+  x = einzusetzender Punkt
   Linie durch x = o + dl mit
-    o = origin
-    d = distance
-    l = Richtung als Einheitsvektor mit Länge 1
-*/
-//Calculate solutions
-float subsquirt = pow((l * (o - c)),2) - pow(abs(o - c), 2) + pow(r,2);
-if(subsquirt < 0){
-  //No solution
-}else if (subsquirt == 0){
-  //Edge
-}else{
-  float d1 = - (l * (o -c)) + sqrt(subsquirt);
-  float d2 = - (l * (o -c)) - sqrt(subsquirt);
-  vec p1 = o + d1 * l;
-  vec p2 = o + d2 * l;
-  if(distance(viewPos, p1) < distance(viewPos))
+  o = origin
+  d = distance
+  l = Richtung als Einheitsvektor mit Länge 1
+  */
+  //Calculate solutions
+  float subsquirt = pow((direction * (viewPos - planetPos)),2) - pow(abs(viewPos - planetPos), 2) + pow(planetRadius,2);
+  if(subsquirt < 0){
+    //No solution
+    return vec4(0,0,0,0);
+    }else if (subsquirt == 0){
+      //EDGE
+      return vec4(1,1,1,0);
+      }else{
+        float d1 = - (direction * (viewPos - planetPos)) + sqrt(subsquirt);
+        float d2 = - (direction * (viewPos - planetPos)) - sqrt(subsquirt);
+        vec3 p1 = viewPos + d1 * direction;
+        vec3 p2 = viewPos + d2 * direction;
+        vec3 renderPoint;
+        if(distance(viewPos, p1) < distance(viewPos, p2)){
+          renderPoint = p1;
+          }else{
+            renderPoint = p2;
+          }
+          //OK, now we know where on the sphere we are, now time to lookup the correct color
+          float lat, lon;
+          lat = tan((renderPoint[0] - planetPos[0]) / (renderPoint[1] - planetPos[1]));
+          lon = tan((renderPoint[2] - planetPos[2]) / (renderPoint[1] - planetPos[1]));
+          //Okay, now transfer to the actual coordinates
+          float x = (lat / (2 * M_PI)) + 0.5;
+          float y = (lon / (2 * M_PI)) + 0.5;
+
+          return vec4(0,x,y,0);
+  }
+
 }
 
+void main(){
+gl_FragColor = vec4(0.0, screenPos, 1.0);
 }
